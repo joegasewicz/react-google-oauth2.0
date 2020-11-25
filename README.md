724b764b89e0152ff6a6f516c0ce55786b95424b
josefgasewicz <joegasewicz@gmail.com>
2020-11-25 00:08:11 +0000

    tidy up

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 React Google OAuth 2.0
 
+Docs: https://joegasewicz.github.io/react-google-oauth2.0/
+
 ## Install
 ```bash
 npm install react-google-oauth2
--- src/authorization.ts
+++ src/authorization.ts
@@ -54,22 +54,7 @@ export class Authorization implements IAuthorization {
         this._googleRedirectURL = value;
     }
 
-    /**
-     * @example
-     *
-     *      https://accounts.google.com/o/oauth2/v2/auth?
-     *      scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&
-     *      access_type=offline&
-     *      include_granted_scopes=true&
-     *      response_type=code&
-     *      state=state_parameter_passthrough_value&
-     *      redirect_uri=https%3A//oauth2.example.com/code&
-     *      client_id=client_id
-     *
-     *      scopes: openid profile email
-     *
-     * @param params
-     */
+    /** @internal */
     public createAuthorizationRequestURL(): void {
         const {
             accessType,
