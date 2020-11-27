d858709c9224ce821425fb67cff79d67e92cd4dd
josefgasewicz <joegasewicz@gmail.com>
2020-11-27 18:31:01 +0000

    docs

--- src/authorization.ts
+++ src/authorization.ts
@@ -2,20 +2,38 @@
 export enum GoogleAPIConnectionStrings {
     GOOGLE_OAUTH_ENDPOINT = "https://accounts.google.com/o/oauth2/v2/auth",
 }
-/** @internal */
+/** @public */
 interface IAuthorizationBase {
+    /** This is your client ID */
     clientId: string;
     /** Where you React app should redirect to if user has successfully logged in **/
     redirectUri: string;
+    /** By default this is set to *code* */
     responseType?: string;
-    /** Valid parameter values are online, which is the default value,
-     * and offline  Set the value to offline if your application needs
-     * to refresh access tokens when the user is not present at the browser
+    /**
+     * Indicates if the api **apiUrl** {@link IGoogleButton} can refresh tokens on the user's
+     * behalf. The default value is *online* but if your backend api will refresh the token then
+     * you must pass in a string value of *offline* (This is recommended for react apps connected
+     * to a REST api backend).
      */
     accessType?: string;
+    /**
+     * This option is currently unavailable but will be in a added in a future version,
+     * TODO - See https://github.com/joegasewicz/react-google-oauth2.0/issues/3
+     * */
     state?: string;
+    /**
+     * Defaulted to true. Any extra scopes the logged in user previously granted
+     * will be available within the new access token.
+     * */
     includeGrantedScopes?: boolean;
+    /** Not required, optional. For prefilling email fields etc. */
     loginHint?: string;
+    /**
+     * Not required, optional. A space-delimited, case-sensitive list of prompts to
+     * present the user. If you don't specify this parameter, the user will be prompted
+     * only the first time your project requests access.
+     * */
     prompt?: string;
 }
 /** @public */
