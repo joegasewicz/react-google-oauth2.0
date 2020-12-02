64262e070b2cb6f2bb4870b89eed28c3a2ea5c26
josefgasewicz <joegasewicz@gmail.com>
2020-12-02 18:38:54 +0000

    bug fix, remove lib

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { IAuthorizationOptions } from "./authorization";
-import { isLoggedIn, logOutOAuthUser, createOAuthHeaders } from "./_apiUtils";
-import { GoogleButton, IGoogleButton } from "./components";
-export { GoogleButton, IGoogleButton, IAuthorizationOptions, isLoggedIn, logOutOAuthUser, createOAuthHeaders, };
